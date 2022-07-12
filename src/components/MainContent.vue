ce4f2b87d009de1ce48ddc1860831045e60f5cf4
Jasur <jasurgofirov@gmail.com>
2022-07-12 18:08:16 +0300

    refactor: MainContent is made dynamically

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -6,35 +6,15 @@
                 <h1 class="font-bold w-4/5 text-3xl my-1.5 text-[#1F1F1F]">Delightfully simple and deceptively powerful task management</h1>
                 <p class="w-4/5 text-[22px] font-normal leading-7 my-1.5 text-[#575757]">30 million+ people organize billions of tasks in Todoist for their work, education, and personal life.</p>
                     <div class="w-full flex flex-wrap justify-between items-center">
-                        <div class="w-[45%] my-4">
-                            <img class="w-[100px] h-[90px]" src="images/icon-1_ad640abe99c49ae1a0c9eb05bb104956.webp" alt="">
-                            <h3 class="my-1.5 font-medium text-2xl">With you everywhere</h3>
-                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Use Todoist’s apps, extensions and widgets on any device or platform.</p>
-                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Download apps</a>
-                        </div>
-                        <div class="w-[45%] my-4">
-                            <img class="w-[100px] h-[90px]" src="images/icon-2_4210d085bc16b95d19ee9605ca20a8d5.webp" alt="">
-                            <h3 class="my-1.5 font-medium text-2xl">There’s a template for that</h3>
-                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Hundreds of templates are available to get you started with whatever’s on your plate.</p>
-                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Start with templates</a>
-                        </div>
-                        <div class="w-[45%] my-4">
-                            <img class="w-[100px] h-[90px]" src="images/icon-3_934c1a18c604c9a66c9b964a973bea5f.webp" alt="">
-                            <h3 class="my-1.5 font-medium text-2xl">Make Todoist yours</h3>
-                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Customize your to-do list with filters, labels, priorities, and more.</p>
-                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> See all features</a>
-                        </div>
-                        <div class="w-[45%] my-4">
-                            <img class="w-[100px] h-[90px]" src="images/icon-4_f546eb9c7155fd7ea242af71babeeeca.webp" alt="">
-                            <h3 class="my-1.5 font-medium text-2xl">Connect with your other tools</h3>
-                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Link Todoist with your calendar, voice assistant, and 30+ other tools.</p>
-                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Install integrations</a>
-                        </div>
-                        <div class="w-[45%] my-4">
-                            <img class="w-[100px] h-[90px]" src="images/icon-5_e69e3f0df7d5163b16ea9cd82df9bec9.webp" alt="">
-                            <h3 class="my-1.5 font-medium text-2xl">Productivity Methods</h3>
-                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Personal productivity recommendations based on your unique traits and strengths.</p>
-                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Take the quiz</a>
+                        <div 
+                        v-for="mainData in mainDatas"
+                        :key="mainData.id"
+                        class="w-[45%] my-4"
+                        >
+                            <img class="w-[100px] h-[90px]" :src="mainData.img" alt="todoist-main-img">
+                            <h3 class="my-1.5 font-medium text-2xl">{{mainData.headTitle}}</h3>
+                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">{{mainData.title}}</p>
+                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">{{mainData.link}}</a>
                         </div>
                     </div>
             </div>
@@ -48,28 +28,35 @@
                             <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">We're in it for the long haul.</a>
                         </p>
                     </div>
-                    <div class="w-[15%]">
-                        <img class="w-[150px]" src="images/icon-6_f82782a12a887c709e39ecf02741af8a.webp" alt="app-download">
-                        <h3 class="my-1 font-bold text-[20px]">30 million+</h3>
-                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">app download</p>
-                    </div>
-                    <div class="w-[15%]">
-                        <img class="w-[150px]" src="images/icon-7_3cdfd11975b2d14cf4ee942720cd1cc7.webp" alt="tasks-completed">
-                        <h3 class="my-1 font-bold text-[20px]">2 billion+</h3>
-                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">tasks completed</p>
-                    </div>
-                    <div class="w-[15%]">
-                        <img class="w-[150px]" src="images/icon-8_3909c68a367c5cce654cd2d9ae010f52.webp" alt="user-location">
-                        <h3 class="my-1 font-bold text-[20px]">160 countries+</h3>
-                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">user locations</p>
-                    </div>
-                    <div class="w-[15%]">
-                        <img class="w-[150px]" src="images/icon-9_a178c84304f35d44c31fbc4aa3f660f5.webp" alt="pro-users">
-                        <h3 class="my-1 font-bold text-[20px]">1 million+</h3>
-                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">pro users</p>
+                    <div 
+                    v-for="mainTask in mainTasks"
+                    :key="mainTask.id"
+                    class="w-[15%]">
+                        <img class="w-[150px]" :src="mainTask.img" alt={{mainTask.title}}>
+                        <h3 class="my-1 font-bold text-[20px]">{{mainTask.headTitle}}</h3>
+                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">{{mainTask.title}}</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
+<script setup  lang="ts">
+import {ref} from 'vue'
+        const mainDatas = ref( [
+                       {id: 1, img: "images/icon-1_ad640abe99c49ae1a0c9eb05bb104956.webp", headTitle: "With you everywhere", title: "Use Todoist’s apps, extensions and widgets on any device or platform.", link: "-> Download apps" },
+                       {id: 2, img: "images/icon-2_4210d085bc16b95d19ee9605ca20a8d5.webp", headTitle: "There’s a template for that", title: "Hundreds of templates are available to get you started with whatever’s on your plate.", link: "-> Start with templates" },
+                       {id: 3, img: "images/icon-3_934c1a18c604c9a66c9b964a973bea5f.webp", headTitle: "Make Todoist yours", title: "Customize your to-do list with filters, labels, priorities, and more.", link: "-> See all features" },
+                       {id: 4, img: "images/icon-4_f546eb9c7155fd7ea242af71babeeeca.webp", headTitle: "Connect with your other tools", title: "Link Todoist with your calendar, voice assistant, and 30+ other tools.", link: "-> Install integrations" },
+                       {id: 5, img: "images/icon-5_e69e3f0df7d5163b16ea9cd82df9bec9.webp", headTitle: "Productivity Methods", title: "Personal productivity recommendations based on your unique traits and strengths.", link: "-> Take the quiz" },
+        ])
+
+        const mainTasks = ref([
+                        {id: 1, img: "images/icon-6_f82782a12a887c709e39ecf02741af8a.webp", headTitle: "30 million+", title: "app download"},    
+                        {id: 2, img: "images/icon-7_3cdfd11975b2d14cf4ee942720cd1cc7.webp", headTitle: "2 billion+", title: "tasks completed"},    
+                        {id: 3, img: "images/icon-8_3909c68a367c5cce654cd2d9ae010f52.webp", headTitle: "160 countries+", title: "user locations"},    
+                        {id: 4, img: "images/icon-9_a178c84304f35d44c31fbc4aa3f660f5.webp", headTitle: "1 million+", title: "pro users"},    
+        ])
+
+</script>
