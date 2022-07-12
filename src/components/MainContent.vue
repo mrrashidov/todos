3821b29bda734e6491410e3e69139fa6562b1ef8
Jasur <jasurgofirov@gmail.com>
2022-07-12 12:19:09 +0300

    feat: mainContent is finished

---
+++ src/components/MainContent.vue
@@ -0,0 +1,75 @@
+<template>
+    <section class="bg-white">
+         <div class="lg:w-[1024px] mx-auto">
+            <div class="w-[60px] h-[1.2px] bg-red-600"></div>
+            <div class="w-full py-8">
+                <h1 class="font-bold w-4/5 text-3xl my-1.5 text-[#1F1F1F]">Delightfully simple and deceptively powerful task management</h1>
+                <p class="w-4/5 text-[22px] font-normal leading-7 my-1.5 text-[#575757]">30 million+ people organize billions of tasks in Todoist for their work, education, and personal life.</p>
+                    <div class="w-full flex flex-wrap justify-between items-center">
+                        <div class="w-[45%] my-4">
+                            <img class="w-[100px] h-[90px]" src="images/icon-1_ad640abe99c49ae1a0c9eb05bb104956.webp" alt="">
+                            <h3 class="my-1.5 font-medium text-2xl">With you everywhere</h3>
+                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Use Todoist’s apps, extensions and widgets on any device or platform.</p>
+                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Download apps</a>
+                        </div>
+                        <div class="w-[45%] my-4">
+                            <img class="w-[100px] h-[90px]" src="images/icon-2_4210d085bc16b95d19ee9605ca20a8d5.webp" alt="">
+                            <h3 class="my-1.5 font-medium text-2xl">There’s a template for that</h3>
+                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Hundreds of templates are available to get you started with whatever’s on your plate.</p>
+                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Start with templates</a>
+                        </div>
+                        <div class="w-[45%] my-4">
+                            <img class="w-[100px] h-[90px]" src="images/icon-3_934c1a18c604c9a66c9b964a973bea5f.webp" alt="">
+                            <h3 class="my-1.5 font-medium text-2xl">Make Todoist yours</h3>
+                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Customize your to-do list with filters, labels, priorities, and more.</p>
+                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> See all features</a>
+                        </div>
+                        <div class="w-[45%] my-4">
+                            <img class="w-[100px] h-[90px]" src="images/icon-4_f546eb9c7155fd7ea242af71babeeeca.webp" alt="">
+                            <h3 class="my-1.5 font-medium text-2xl">Connect with your other tools</h3>
+                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Link Todoist with your calendar, voice assistant, and 30+ other tools.</p>
+                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Install integrations</a>
+                        </div>
+                        <div class="w-[45%] my-4">
+                            <img class="w-[100px] h-[90px]" src="images/icon-5_e69e3f0df7d5163b16ea9cd82df9bec9.webp" alt="">
+                            <h3 class="my-1.5 font-medium text-2xl">Productivity Methods</h3>
+                            <p class="text-base font-normal leading-7 my-1.5 text-[#575757]">Personal productivity recommendations based on your unique traits and strengths.</p>
+                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">-> Take the quiz</a>
+                        </div>
+                    </div>
+            </div>
+            <div class="w-[60px] h-[1.2px] bg-red-600"></div>
+            <div class="w-full py-8">
+                <div class="w-full flex justify-between items-center">
+                    <div class="w-[30%]">
+                        <h1 class="font-bold text-3xl my-1.5">A task manager you can trust for life</h1>
+                        <p class="text-[18px] font-normal leading-7 my-1.5 text-[#575757]">
+                            We’ve been building Todoist for 15 years and 160 days.
+                            <a class="no-underline text-1xl text-[#378AEE] hover:underline" href="">We're in it for the long haul.</a>
+                        </p>
+                    </div>
+                    <div class="w-[15%]">
+                        <img class="w-[150px]" src="images/icon-6_f82782a12a887c709e39ecf02741af8a.webp" alt="app-download">
+                        <h3 class="my-1 font-bold text-[20px]">30 million+</h3>
+                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">app download</p>
+                    </div>
+                    <div class="w-[15%]">
+                        <img class="w-[150px]" src="images/icon-7_3cdfd11975b2d14cf4ee942720cd1cc7.webp" alt="tasks-completed">
+                        <h3 class="my-1 font-bold text-[20px]">2 billion+</h3>
+                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">tasks completed</p>
+                    </div>
+                    <div class="w-[15%]">
+                        <img class="w-[150px]" src="images/icon-8_3909c68a367c5cce654cd2d9ae010f52.webp" alt="user-location">
+                        <h3 class="my-1 font-bold text-[20px]">160 countries+</h3>
+                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">user locations</p>
+                    </div>
+                    <div class="w-[15%]">
+                        <img class="w-[150px]" src="images/icon-9_a178c84304f35d44c31fbc4aa3f660f5.webp" alt="pro-users">
+                        <h3 class="my-1 font-bold text-[20px]">1 million+</h3>
+                        <p class="text-base font-normal leading-7 my-1 text-[#575757]">pro users</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
