d5e18099a143f830b4be8a4eeb127bbeea29778f
Nasrulloev Rustam <exellentgo3@gmail.com>
2022-07-13 18:23:40 +0500

    fix: home main sections added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <Header />
+  <MainContentImg />
+  <MainContent />
   <div class="lg:w-[1200px] mx-auto"></div>
   <Footer />
 </template>
@@ -7,11 +9,15 @@
 <script lang="ts">
 import Header from "@/components/Header.vue";
 import Footer from "@/components/footer.vue";
+import MainContent from "@/components/MainContent.vue";
+import MainContentImg from "@/components/MainContentImg.vue";
 
 export default {
   components: {
     Header,
     Footer,
+    MainContent,
+    MainContentImg,
   },
 };
 </script>
