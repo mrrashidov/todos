b2409a6ebaa3a2f4d795e4e1d44d980b0cb27e4a
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2022-07-19 09:22:33 +0300

    feat: features roots created

--- src/routers/routes/root.ts
+++ src/routers/routes/root.ts
@@ -2,10 +2,12 @@ export const root = [
   { path: "/", component: () => import("@/views/Home.vue") },
   { path: "/auth/login", component: () => import("@/views/Login.vue") },
   { path: "/auth/signup", component: () => import("@/views/SignUp.vue") },
+  { path: "/features", component: () => import("@/views/Features.vue") },
   {
     path: "/app/project",
     component: () => import("@/components/AppProject.vue"),
   },
+  
   {
     path: "/app/onboard/usecase",
     component: () => import("@/components/Register/OnboardUseCase.vue"),
